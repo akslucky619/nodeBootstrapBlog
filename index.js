fa1f4fe87e6b19c2033d7d38e6269b161bfe222f
akash@619 <Akash.sharma1@in.ey.com>
2021-02-05 00:15:51 +0530

    add edge

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const path = require("path");
 
+const { engine } = require("express-edge");
+
 const express = require("express");
 
 const app = new express();
 
 app.use(express.static("public"));
+app.use(engine);
+
+app.set("views", `${__dirname}/views`);
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "pages/index.html"));
